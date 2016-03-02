76e167657154e39b6d67eeccb1b717984d04283a
unknown <eboateng@GIS-LTP-11.cgc.local>
2016-03-02 14:44:59 -0500

    3/2/2016 - sunshine

--- app/src/main/java/app/com/example/eboateng/sunweather/MainActivityFragment.java
+++ app/src/main/java/app/com/example/eboateng/sunweather/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +22,21 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+//creation of array list and fake data boy.
+        ArrayList<String> weather = new ArrayList<String>();
+        weather.add("Today - Sunny - 88 / 63");
+        weather.add("Tomorrow - Foggy - 70 / 46");
+        weather.add("Weds - Cloudy - 72 / 63");
+        weather.add("Thurs - Raint - 64 / 51");
+        weather.add("Fri - Foggy - 70 / 46");
+        weather.add("Sat - Sunny - 76 / 68");
+// creation of an arrayadapter. First receives context points to a layout with selected object data
+  //      ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        //        android.R.layout.activity_list_item, weather);
+
+      //  ListView listView = (ListView) MainActivityFragment.this.findViewById(R.id.listview);
+      //  listView.setAdapter(adapter);
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
